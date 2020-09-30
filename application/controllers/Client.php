b6c0c3ea72948de1342a5798150fdf09e8d1f39f
ajosav <ajosavboy@gmail.com>
2020-09-30 14:10:40 +0100

    added source XML

--- application/controllers/Client.php
+++ application/controllers/Client.php
@@ -2,13 +2,11 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+class Loan_account extends CI_Controller {
 
-
-
-class Client  extends CI_Controller {
     public $mambu_base_url;
     private $today;
-    public function __construct() {
+	public function __construct() {
 		
         parent::__construct();
         
@@ -21,24 +19,115 @@ class Client  extends CI_Controller {
         $this->load->model('Base_model'); 
         
         
-		$this->mambu_base_url = $this->config->item('rnm_mambu_base_url');
+        $this->mambu_base_url = $this->config->item('rnm_mambu_base_url');
+        
+        parent::__construct();
+        header('Access-Control-Allow-Origin: *');
+        header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, access-control-allow-origin");
+        header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
 
     }
 
-
-    public function repayment_schedule() {
+    //Method for app installation on MAMBU
+    public function install() {
         
     }
 
-
-    public function verify_otp() {
-
+    //Method for app uninstallation on MAMBU
+    public function uninstall() {
+        
     }
 
-    public function view_otp() {
+    public function index() {
+        echo "Welcome to Part liquidation App";
+    }
+
+   
+    private function get_next_repayment_due_date($loan_id) {
+        $repayments = json_decode($this->Base_model->get_repayments($loan_id));
+
+        foreach($repayments as $index => $repayment) {
+            if($repayment->dueDate > $this->today) {
+                return $repayments[$index];
+            } 
+        }
+
+        return null;
+    }
+
+    private function get_outstanding_repayments($loan_id) {
+        $repayments = json_decode($this->Base_model->get_repayments($loan_id));
+        $outstanding_repayments = [];
+        foreach($repayments as $repayment) {
+            if((string) $repayment->state != "PAID") {
+                $outstanding_repayments[] = $repayment;
+            }
+        }
+
+        return $outstanding_repayments;
+    }
+
+    private function get_max_available_tenor($loan_id) {
+        $repayments = json_decode($this->Base_model->get_repayments($loan_id));
+        $available_tenor = [];
+        foreach($repayments as $repayment) {
+            if((string) $repayment->state != "PAID" && (string) $repayment->state != "LATE") {
+                $available_tenor[] = $repayment;
+            }
+        }
+
+        return $available_tenor;
+    }
+
+  
+    public function schedule_review($schedule_id) {
+        if($loan_schedule = $this->Base_model->find("loan_schedule", ['schedule_id' => $schedule_id])) {
+            $repayment_schedule = $this->Base_model->selectRepayment($loan_schedule->schedule_id);
+            $data = [
+                "loan_schedule" => $loan_schedule,
+                "repayment_schedule" => $repayment_schedule
+            ];
+            
+            
+
+            $this->load->view('part_liquidation/meta_link');
+            $this->load->view('part_liquidation/preview_schedule_view', $data); 
+            $this->load->view('part_liquidation/footer_link');
+        } else {
+            redirect(base_url().'Loan_account/start');
+        }
+    }
+
+
+    public function send_schedule_to_customer() {
+        $client_key = $this->input->post('accountHolderKey');
+        $loan = $this->input->post('loan_id');
+        $schedule_id = $this->input->post('schedule_id');
+        $liquidationAmount = number_format($this->input->post('liquidationAmount'));
+        $endpointURL = $this->mambu_base_url . "api/clients/" . $client_key ."?fullDetails=true";
+        $response = $this->Base_model->call_mambu_api_get($endpointURL);
+			
+        $client_details = json_decode($response, TRUE);
+        
+        $personal_email = $client_details['client']['emailAddress'];
+        $client_name = $client_details['client']['firstName'];
+        $link = base_url() . "/view_schedule";
+
+        $this->Base_model->notifyMail(["{$personal_email}"], "Part-liquidation of your Loan Account", "
+            <p> <img src = 'https://www.renmoneyng.com/images/uploads/email-template-top.png' alt = '' /> </p>
+            <p>Dear {$client_name}, </p>
+            <p>Please find attached the New repayment schedule based on your recent bulk payment N{$liquidationAmount} for your loan with ID {$loan} <br>
+            {$link} <br>
+            Click on the Link above to Accept or Reject.
+            For any enquiries, contact hello@renmoney.com</p>
+            <p>Thank you for choosing RenMoney MFB LTD. 
+            <br> <p>  <img src = 'https://www.renmoneyng.com/images/uploads/email-template-bottom.png ' alt = '' /> </p>
+        ");
+        
         $this->load->view('part_liquidation/meta_link');
-		$this->load->view('part_liquidation/otp_verification'); 
-		$this->load->view('part_liquidation/footer_link');
+        $this->load->view('part_liquidation/email_successfully_sent'); 
+        $this->load->view('part_liquidation/footer_link');
     }
+	
 
 }
---
+++ application/views/part_liquidation/client/preview_schedule.php
@@ -0,0 +1,128 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+        <section class="main-content container">
+    
+            <!--page header end-->
+				<div class="col-lg-12">
+                    <!-- START panel-->
+                    <div class="panel panel-default">
+                        <div class="panel-heading">Repayment Schedule</div>
+                            <div class="panel-body">
+                                <form class="form-horizontal" method="post" action="<?= base_url() ?>Loan_account/send_schedule_to_customer" style="padding: 10px 40px;">
+                    
+                                    <div class="row">
+                                        <div class="form-row">
+                                            <div class="form-group">
+                                                <div class="col-md-4">
+                                                    <label for="data_file">Loan ID</label>
+                                                    <input type="hidden" name="accountHolderKey" id="accountHolderKey" value="<?= $loan_schedule->accountHolderKey?>">
+                                                    <input type="hidden" name="schedule_id" id="schedule_id" value="<?= $loan_schedule->schedule_id?>">
+                                                    <input type="email"  placeholder="Loan ID" name="loan_id" class="form-control" value="<?= $loan_schedule->loan_id ?>" title="Client Loan ID" readonly>
+                                                </div>
+                                                <div class="col-md-8">
+                                                    <label for="date_generated">Liquidation Amount</label>
+                                                    <input type="text"  placeholder="Liquidation Amount" name="liquidationAmount" class="form-control" value="<?= $loan_schedule->liquidationAmount ?>" title="Liquidation Amount" readonly>
+                                                </div>
+                                            </div>
+                                            <div class="form-group">
+                                                <div class="col-lg-4">
+                                                    <label for="date_generated">Principal Balance (After Liquidation)</label>
+                                                    <input type="text"  placeholder="Interest Rate" name="principal_balance" class="form-control" value="<?= $loan_schedule->principalBalance ?>" title="Principal Balance" readonly>
+                                                </div>
+                                                <div class="col-lg-4">
+                                                    <label for="date_generated">Loan Tenure</label>
+                                                    <input type="text"  placeholder="Interest Accrued" name="interest_accrued" class="form-control" value="<?= $loan_schedule->tenure ?>" title="Interest Accrued" readonly>
+                                                </div>
+                                                <div class="col-lg-4">
+                                                    <label for="date_generated">Status</label>
+                                                    <input type="text"  placeholder="Intrest Overdue" name="interest_overdue" class="form-control" value="
+                                                        <?php
+                                                            switch($loan_schedule->status) {
+                                                                    case('0') :
+                                                                        echo "Pending User Confirmation";
+                                                                    break;
+                                                                    case('1'):
+                                                                        echo "Awaiting Client Approval";
+                                                                    break;
+                                                                    case('2'):
+                                                                        echo "Applied on loan";
+                                                                    break;
+                                                                    case('3'):
+                                                                        echo "Rejected By Client";
+                                                                    break;
+                                                                    default:
+                                                                        echo "Under Processing";
+                    
+                                                                } 
+                                                        ?> 
+                                                    title="Interest Overdue" readonly>
+                                                </div>
+
+                                            </div>
+                                            <div class="form-group">
+                                                <div class="col-lg-4">
+                                                    <label for="date_generated">Payment Status</label>
+                                                    <input type="text"  placeholder="Payment Status" name="payment_status" class="form-control" value="<?= $loan_schedule->paymentStatus ?>" title="Payment Status" readonly>
+                                                </div>
+                                                <div class="col-lg-4">
+                                                    <label for="date_generated">Date Generated</label>
+                                                    <input type="text"  placeholder="Date Generated" name="date_generated" class="form-control" value="<?= $loan_schedule->date_generated ?>" title="Date Generated" readonly>
+                                                </div>
+                                                
+                                            </div>
+                                            <table class="table table-striped dt-responsive nowrap">
+                                                <thead>
+                                                    <tr>
+                                                        <th>S/N</th>
+                                                        <th>Due Date</th>
+                                                        <th>Principal Due</th>
+                                                        <th>Interest Due</th>
+                                                        <th>Fees Due</th>
+                                                        <th>Penalty Due</th>
+                                                        
+                                                    </tr>
+                                                </thead>
+
+                                                <tbody> 
+                                                    <?php if(count($repayment_schedule) > 0): ?>    
+
+                                                        <?php $sn = 1; foreach($repayment_schedule as $repayments): ?> 
+                                                            <tr>
+                                                                <td><?= $sn++; ?></td>
+                                                                <td><?= $repayments['dueDate'];?></td>
+                                                                <td><?= number_format($repayments['principalDue'], 2); ?></td>
+                                                                <td><?= number_format($repayments['interestDue'], 2); ?></td>
+                                                                <td><?= number_format($repayments['feesDue'], 2); ?></td>
+                                                                <td><?= number_format($repayments['penaltyDue'], 2); ?></td>
+                                                            </tr>
+                                                        
+                                                        <?php endforeach ?> 
+                                                    <?php endif ?>
+                                                </tbody>
+                                            </table>
+                                            <div class="form-group">
+                                                <div class="col-lg-12 text-center">
+                                                    <button type="submit" class="btn btn-success" style="margin:5px;" id="recalcaculate_schedule" title="Recalculate Client Loan Repayment Schedule">Send To Customer Email</button>
+                                                </div>
+                                            </div>
+
+                                        </div>
+
+                                    </div>
+                                    
+                                    
+                                    
+
+                                </form>
+                            </div>
+                        </div>
+                    <!-- END panel-->
+                    </div>
+ 
+                </div>
+        </section>
+	
+
+		
+		
+		
