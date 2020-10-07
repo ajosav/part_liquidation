d3e59fe5167b8362f23f7c54b4902042d6307f9c
ajosav <ajosavboy@gmail.com>
2020-10-07 12:11:15 +0100

    Debugging at loan schedule

--- application/controllers/Client.php
+++ application/controllers/Client.php
@@ -38,7 +38,7 @@ class Client extends CI_Controller {
         $rate = 13.81; $nper = 12; $pv = 500000.00; $fv = 0; $type = 0; $fee_rate = (0.00 / 100);
         
 
-        $schedule_id = 'sch5f7d87999c026';
+        $schedule_id = 'sch5f7da11d9f9c4';
         $loan_id = '30380087';
 
         $loan_schedule = $this->Base_model->find("loan_schedule", ['schedule_id' => $schedule_id]);
